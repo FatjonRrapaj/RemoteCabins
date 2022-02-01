a8be3ae600b7f9894d81a9aa325da880ee23aa3b
Fatjon Rrapaj <38084457+FatjonRrapaj@users.noreply.github.com>
2022-02-01 23:12:24 +0100

    added home function declarations

--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -1,7 +1,7 @@
-import { MouseEvent, useEffect } from 'react';
+import React, { MouseEvent, useEffect, useState, useRef, InputHTMLAttributes } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { signOut } from 'firebase/auth';
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc, onSnapshot, doc, CollectionReference } from 'firebase/firestore';
 
 import { useFirebase } from '../../firebase';
 import { handleAuthErrorCode, handleDbErrorCode } from '../../firebase/errorMessageHandler';
@@ -14,24 +14,33 @@ type Cabin = {
 export default function Home() {
   const navigate = useNavigate();
   const { firebaseAuth, database } = useFirebase();
-
-  const onAddHomeSubmitted = async ({ name, location }: Cabin) => {
-    try {
-      const docRef = await addDoc(collection(database, 'cabins'), {
-        name,
-        location,
-      });
-      console.log('docRef: ', docRef);
-    } catch (error: any) {
-      handleDbErrorCode(error.code);
-    }
-  };
+  const [cabins, setCabins] = useState<Cabin[] | []>([]);
+  const [addCabinModalVisible, setAddCabinModalVisible] = useState<Boolean>(false);
+  const [cabinName, setCabinName] = useState<string>('');
+  const [cabinLocation, setCabinLocation] = useState<string>('');
+  const [loadingSavingCabin, setLoadingSavingCabin] = useState<boolean>(false);
 
   useEffect(() => {
-    console.log('database: ', database);
+    const unsubscribeCabinsChanges = onSnapshot(
+      collection(database, 'cabins'),
+      (collection) => {
+        console.log('cabinsCollection: ', collection.docs);
+        const cabins = collection.docs.map((element) => {
+          return element;
+        });
+        setCabins(cabins as unknown as Cabin[]);
+      },
+      (error) => {
+        console.error('cabinsCollectionChanges error: ', error);
+      },
+    );
+
+    return () => {
+      unsubscribeCabinsChanges();
+    };
   }, []);
 
-  function handleButtonClick(event: MouseEvent): void {
+  function handleSignOut(event: MouseEvent): void {
     event.preventDefault();
     signOut(firebaseAuth)
       .then(() => {
@@ -43,5 +52,92 @@ export default function Home() {
       });
   }
 
-  return <button onClick={handleButtonClick}>Sign out</button>;
+  function handleAddCabinPressed(event: MouseEvent): void {
+    event.preventDefault();
+    //open modal with form and cabin data
+    setAddCabinModalVisible(true);
+  }
+
+  function onCabinNameChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    event.preventDefault();
+    setCabinName(event.target.value);
+  }
+
+  function onCabinLocationChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    event.preventDefault();
+    setCabinLocation(event.target.value);
+  }
+
+  async function handleSaveCabinSubmit(event: React.ChangeEvent<HTMLFormElement>) {
+    event.preventDefault();
+    try {
+      const docRef = await addDoc(collection(database, 'cabins'), {
+        cabinName,
+        cabinLocation,
+      });
+      alert('Successfully added cabin');
+      setCabinName('');
+      setCabinLocation('');
+      console.log('docRef: ', docRef);
+    } catch (error: any) {
+      handleDbErrorCode(error.code);
+    }
+  }
+
+  function renderModal(): JSX.Element {
+    return (
+      <div className="wrapper bg-grey-600 opacity-5">
+        <div className="card">
+          <form onSubmit={handleSaveCabinSubmit}>
+            <label>
+              <input
+                required
+                className="input"
+                placeholder="Enter cabin name"
+                onChange={onCabinNameChange}
+                value={cabinName}
+              />
+              <input
+                required
+                className="input"
+                placeholder="Enter location name"
+                onChange={onCabinLocationChange}
+                value={cabinLocation}
+              />
+              <button
+                disabled={loadingSavingCabin}
+                className="bg-green-500 rounded-sm self-end align-bottom p-2 mt-6 text-white font-semibold"
+              >
+                {loadingSavingCabin ? 'Loading...' : 'Save Cabin'}
+              </button>
+            </label>
+          </form>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="wrapper items-start">
+      <div className="card self-start w-4/5 mt-40">
+        {cabins.length === 0 && <>there is not any cabin added yet</>}
+        {cabins.map(({ name, location }) => {
+          return <div>{name}</div>;
+        })}
+        <button
+          className="bg-green-500 rounded-sm self-end align-bottom p-2 mt-6 text-white font-semibold"
+          onClick={handleAddCabinPressed}
+        >
+          Add a new cabin
+        </button>
+      </div>
+      <button
+        className="bg-red-500 rounded-sm self-end align-bottom p-2 mb-6 text-white font-semibold"
+        onClick={handleSignOut}
+      >
+        Sign out
+      </button>
+      {addCabinModalVisible && renderModal()}
+    </div>
+  );
 }
